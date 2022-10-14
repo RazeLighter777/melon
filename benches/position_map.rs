66d763dbf2a1f5c15379a6f1b31a5cc7570ff4ff
Suess <justin.suess@westpoint.edu>
2022-10-14 11:42:00 -0400

    fixing benches

---
+++ benches/position_map.rs
@@ -0,0 +1,62 @@
+#![feature(const_type_name)]
+#![feature(associated_type_bounds)]
+#![feature(test)]
+extern crate test;
+use melon::*;
+use test::Bencher;
+
+struct TestSystem {}
+impl system::System for TestSystem {
+    fn query(&self) -> query::Query {
+        query::QueryBuilder::new()
+            .with::<base_components::Position>()
+            .build()
+    }
+    fn execute(
+        &self,
+        query_result: &mut query::QueryResult,
+        world: &world::World,
+    ) -> commands::Command {
+        for e in query_result.iter() {
+            println!(
+                "Entity has position {}",
+                e.get_unchecked::<base_components::Position>().x
+            );
+            e.set::<base_components::Position>(base_components::Position {
+                x: e.get_unchecked::<base_components::Position>().x + 1,
+                y: e.get_unchecked::<base_components::Position>().y + 1,
+            });
+        }
+        commands::Command {}
+    }
+}
+
+
+
+#[bench]
+fn position_map_test(b: &mut Bencher) {
+    let mut world = default_world::DefaultWorld::new().build();
+    world
+        .add_entity()
+        .with(base_components::Position { x: 0, y: 0 })
+        .spawn();
+    world
+        .add_entity()
+        .with(base_components::Position { x: 0, y: 0 })
+        .spawn();
+    for i in 0..10000 {
+        world
+            .add_entity()
+            .with(base_components::Position { x: i, y: 0 })
+            .with(base_components::Name {
+                name: "test".to_string(),
+            })
+            .spawn();
+    }
+    let stage1 = stage::StageBuilder::new()
+        .with_system(TestSystem {})
+        .build();
+    b.iter(|| {
+        world.execute_stage(&stage1);
+    })
+}
